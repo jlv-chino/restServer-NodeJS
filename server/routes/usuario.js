eb981368ffa16668bd24d4afde2fecbc5de14de8
Jose Luis Vilches <jose.luis.comvilches@gmail.com>
2021-01-20 01:20:21 -0300

    Agregado de libreria underscore + filtrado de datos

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -1,11 +1,37 @@
 const express = require('express')
 const bcrypt = require('bcrypt')
+const _ = require('underscore')
 const Usuario = require('../models/usuario')
 
 const app = express()
 
 app.get('/usuario', function(req, res){
-    res.json('get usuario')
+
+    let desde = req.query.desde || 0
+    desde = Number(desde)
+
+    let limite = req.query.limite || 5
+    limite = Number(limite)
+
+    Usuario.find({}, 'nombre email role estado google img')
+            .skip(desde)
+            .limit(limite)
+            .exec((err, usuarios) =>{
+                if(err){
+                    return res.status(400).json({
+                        ok:false,
+                        err
+                    })
+                } 
+
+                Usuario.count({}, (err, conteo) =>{
+                    res.json({
+                        ok: true,
+                        usuarios,
+                        cuantos: conteo
+                    })
+                })
+            })
 })
 
 app.post('/usuario', function(req, res){
@@ -38,9 +64,9 @@ app.post('/usuario', function(req, res){
 app.put('/usuario/:id', function(req, res){
 
     let id= req.params.id
-    let body = req.body
+    let body = _.pick(req.body, ['nombre', 'email', 'img', 'role', 'estado'])
 
-    Usuario.findByIdAndUpdate(id, body, {new: true}, (err, usuarioDB) =>{
+    Usuario.findByIdAndUpdate(id, body, {new: true, runValidators: true}, (err, usuarioDB) =>{
         if(err){
             return res.status(400).json({
                 ok:false,
