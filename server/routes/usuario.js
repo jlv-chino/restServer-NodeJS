afe0ed4013394629c36ded7ffbb189f0ebc6b075
Jose Luis Vilches <jose.luis.comvilches@gmail.com>
2021-01-21 02:59:53 -0300

    Prueba en remoto, pero creo que heroku no funciona con mongoDB

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -81,34 +81,6 @@ app.put('/usuario/:id', function(req, res){
     })
 })
 
-/*app.delete('/usuario/:id', function(req, res){
-
-    let id = req.params.id
-
-    Usuario.findByIdAndRemove(id, (err, usuarioBorrado) =>{
-        if(err){
-            return res.status(400).json({
-                ok:false,
-                err
-            })
-        }
-
-        if(!usuarioBorrado){
-            return res.status(400).json({
-                ok:false,
-                err:{
-                    message: 'Usuario no encontrado'
-                }
-            })
-        }
-
-        res.json({
-            ok: true,
-            usuario: usuarioBorrado
-        })
-    })
-})*/
-
 app.delete('/usuario/:id', function(req, res){
 
     let id = req.params.id
