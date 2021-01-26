4648caf52a4b5a53922de2da72b0f6e85ade6d08
Jose Luis Vilches <jose.luis.comvilches@gmail.com>
2021-01-26 01:17:43 -0300

    Varaible de entorno garegada

--- server/config/config.js
+++ server/config/config.js
@@ -8,7 +8,7 @@ let urlDB
 if (process.env.NODE_ENV === 'dev'){
    urlDB = 'mongodb://localhost:27017/cafe'
 } else {
-   urlDB = 'mongodb+srv://chino:CgAPngw73Q39llz0@cluster0.n9k16.mongodb.net/test?retryWrites=true&w=majority'
+   urlDB = process.env.MONGO_URI
 }
 
 process.env.URLDB = urlDB
