b2b911653e751293d998b65d73ab678a7cd56585
Jose Luis Vilches <jose.luis.comvilches@gmail.com>
2021-01-26 00:59:30 -0300

    no funciona

--- server/config/config.js
+++ server/config/config.js
@@ -6,7 +6,7 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
 let urlDB
 
 if (process.env.NODE_ENV === 'dev'){
-    urlDB = 'mongodb://localhost:27017/cafe'
+   urlDB = 'mongodb://localhost:27017/cafe'
 } else {
    urlDB = 'mongodb+srv://chino:CgAPngw73Q39llz0@cluster0.n9k16.mongodb.net/test?retryWrites=true&w=majority'
 }
