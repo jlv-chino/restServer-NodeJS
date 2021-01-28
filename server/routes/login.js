d16dfe600c31df47d17d9a1466787b23a9ddeb60
Jose Luis Vilches <jose.luis.comvilches@gmail.com>
2021-01-28 01:51:49 -0300

    Agregado el login pero sin JWT

--- server/routes/login.js
+++ server/routes/login.js
@@ -7,9 +7,41 @@ const Usuario = require('../models/usuario')
 const app = express()
 
 app.post('/login', (req, res) =>{
-    res.json({
-        ok: true
+    let body = req.body
+    Usuario.findOne({ email: body.email}, (err, usuarioDB) =>{
+        if(err){
+            return res.status(500).json({
+                ok: false,
+                err
+            })
+        }
+
+        if(!usuarioDB){
+            return res.status(400).json({
+                ok: false,
+                err:{
+                    message: 'Usuario y/o password incorrectos'
+                }
+            })
+        }
+
+       if(!bcrypt.compareSync(body.password, usuarioDB.password)) {
+            return res.status(400).json({
+                ok: false,
+                err:{
+                    message: 'Usuario y/o password incorrectos'
+                }
+            })
+       }
+
+       res.json({
+           ok: true,
+           usuario: usuarioDB,
+           token: '123'
+       })
+
     })
+
 })
 
 module.exports = app
