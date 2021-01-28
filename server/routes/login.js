7672985a01d13c82aa2a6a0acc7cba55cae571fe
Jose Luis Vilches <jose.luis.comvilches@gmail.com>
2021-01-28 01:24:58 -0300

    Agregado de rutas login e index, todo funciona solamente en desarrollo

---
+++ server/routes/login.js
@@ -0,0 +1,15 @@
+const express = require('express')
+
+const bcrypt = require('bcrypt')
+
+const Usuario = require('../models/usuario')
+
+const app = express()
+
+app.post('/login', (req, res) =>{
+    res.json({
+        ok: true
+    })
+})
+
+module.exports = app
--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json())
 
-app.use(require('./routes/usuario'))
+app.use(require('./routes/index'))
 
 mongoose.connect(process.env.URLDB, (err, res)=>{
 
