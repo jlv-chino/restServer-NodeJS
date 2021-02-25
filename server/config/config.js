3548a86d2345ee082e13452e675208009d9b0fe8
Jose Luis Vilches <jose.luis.comvilches@gmail.com>
2021-02-25 02:04:13 -0300

    Dejamos como estaba anteriormente ruta de producción

--- server/config/config.js
+++ server/config/config.js
@@ -12,7 +12,7 @@ let urlDB
 if (process.env.NODE_ENV === 'dev'){
    urlDB = 'mongodb://localhost:27017/cafe'
 } else {
-   urlDB = 'mongodb+srv://chino:1VGj3jOSKlvwJiO2@cluster0.n9k16.mongodb.net/test?retryWrites=true&w=majority'
+   urlDB = process.env.MONGO_URI
 }
 
 process.env.URLDB = urlDB
