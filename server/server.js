2e4c6150c8ac3dc54a07208d85f35483e8564ece
Jose Luis Vilches <jose.luis.comvilches@gmail.com>
2021-03-05 02:34:03 -0300

    Agregamos google sign in

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ require('./config/config')
 
 const express = require('express')
 const mongoose = require('mongoose')
+const path = require('path')
 
 const app = express()
 
@@ -9,6 +10,8 @@ const bodyParser = require('body-parser')
 
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json())
+
+app.use(express.static(path.resolve(__dirname, '../public')))
 
 app.use(require('./routes/index'))
 
