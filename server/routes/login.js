b6f032a3de96bfadd62b18d3674ae78ae9f51270
José Luis Vilches <jose.luis.comvilches@gmail.com>
2021-03-06 03:24:59 -0300

    subimos las rutas a la nube de postman, funciona google signin token

--- server/routes/login.js
+++ server/routes/login.js
@@ -57,22 +57,85 @@ async function verify(token) {
     });
     const payload = ticket.getPayload();
 
-    console.log(payload.name);
-    console.log(payload.email);
+    return{
+        nombre: payload.name,
+        email: payload.email,
+        img: payload.picture,
+        google: true
+    }
   }
 
   // verify().catch(console.error);
 
-app.post('/google', (req, res) =>{
+app.post('/google', async(req, res) =>{
 
     let token = req.body.idtoken
 
-    verify(token)
+    let googleUser = await verify(token)
+                        .catch( e =>{
+                            return res.status(403).json({
+                                ok: false,
+                                err: e
+                            })
+                        })
 
-    res.json({
-        token
-    })
+    Usuario.findOne({email: googleUser.email}, (err, usuarioDB) =>{
+        if (err){
+            return res.status(500).json({
+                ok: false,
+                err
+            })
+        }
 
+        if (usuarioDB){
+            if (usuarioDB.google === false){
+                return res.status(400).json({
+                    ok: false,
+                    err:{
+                        message: 'Usar autenticación normal'
+                    }
+                }) 
+            }else{
+                let token = jwt.sign({
+                    usuario: usuarioDB
+                }, process.env.SEED, {expiresIn: process.env.CADUCIDAD_TOKEN})
+
+                return res.json({
+                    ok: true,
+                    usuario: usuarioDB,
+                    token
+                })
+            }
+        }else{
+            let usuario = new Usuario()
+
+            usuario.nombre = googleUser.nombre
+            usuario.email = googleUser.email
+            usuario.img = googleUser.img
+            usuario.google = true
+            usuario.password = ':)'
+
+            usuario.save( (err, usuarioDB) =>{
+                if (err){
+                    return res.status(500).json({
+                        ok: false,
+                        err
+                    })
+                }
+
+                let token = jwt.sign({
+                    usuario: usuarioDB
+                }, process.env.SEED, {expiresIn: process.env.CADUCIDAD_TOKEN})
+
+                return res.json({
+                    ok: true,
+                    usuario: usuarioDB,
+                    token
+                })
+            })
+
+        }
+    })                    
 })
 
 module.exports = app
