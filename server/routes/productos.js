6f03f0741509d8b13dbe19bb47cc1d15db4bb55c
José Luis Vilches <jose.luis.comvilches@gmail.com>
2021-03-26 02:06:28 -0300

    Completamos el crud de los productos

---
+++ server/routes/productos.js
@@ -0,0 +1,180 @@
+const express = require('express')
+const {verificaToken} = require('../middlewares/autenticacion')
+
+let app = express()
+let Producto = require('../models/producto')
+
+//*** Listar todas los productos ***/
+app.get('/productos', verificaToken, (req, res)=>{
+    let desde = req.query.desde || 0
+    desde = Number(desde)
+
+    Producto.find({disponible: true})
+            .skip(desde)
+            .limit(5)
+            .populate('usuario', 'nombre email')
+            .populate('categoria', 'descripcion')
+            .exec((err, productos)=>{
+                if(err){
+                    return res.status(500).json({
+                        ok: false,
+                        err
+                    })
+                }
+
+                res.json({
+                    ok: true,
+                    productos
+                })
+            })
+})
+
+//*** Listar un producto por ID ***/
+app.get('/productos/:id', verificaToken, (req, res)=>{
+    let id = req.params.id
+
+    Producto.findById(id)
+            .populate('usuario', 'nombre email')
+            .populate('categoria', 'nombre')
+            .exec((err, productoDB)=>{
+                if(err){
+                    return res.status(500).json({
+                     ok: false,
+                     err
+                    })
+                }
+
+                if(!productoDB){
+                    return res.status(400).json({
+                     ok: false,
+                     err:{
+                         message: 'El Id no existe'
+                     }
+                    })
+                }
+
+                res.json({
+                    ok: true,
+                    producto: productoDB
+                })
+            })
+    
+    
+})
+
+//*** Crear un producto ***/
+app.post('/productos', verificaToken, (req, res)=>{
+    let body = req.body
+    let producto = new Producto({
+        usuario: req.usuario._id,
+        nombre: body.nombre,
+        precioUni: body.precioUni,
+        descripcion: body.descripcion,
+        disponible: body.disponible,
+        categoria: body.categoria,
+    })
+
+    producto.save((err, productoDB)=>{
+        if(err){
+            return res.status(500).json({
+                ok: false,
+                err
+            })
+        }
+
+        res.status(201).json({
+            ok: true,
+            producto: productoDB
+        })
+
+
+    })
+})
+
+//*** Actualizar un producto ***/
+app.put('/productos/:id', verificaToken, (req, res)=>{
+
+    let id = req.params.id
+    let body = req.body
+
+    Producto.findById(id, (err, productoDB)=>{
+        if(err){
+            return res.status(500).json({
+                ok: false,
+                err
+            })
+        }
+
+        if(!productoDB){
+            return res.status(400).json({
+                ok: false,
+                err:{
+                    message: 'El Id no existe'
+                }
+            })
+        }
+
+        productoDB.nombre = body.nombre
+        productoDB.precioUni = body.precioUni
+        productoDB.categoria = body.categoria
+        productoDB.descripcion = body.descripcion
+        productoDB.disponible = body.disponible
+
+        productoDB.save((err, productoGuardado)=>{
+            if(err){
+                return res.status(500).json({
+                    ok: false,
+                    err
+                })
+            }
+
+            res.json({
+                ok: true,
+                producto: productoGuardado
+            })
+        })
+    })
+})
+
+//*** Eliminar un producto ***/
+app.delete('/productos/:id', verificaToken, (req, res)=>{
+    let id = req.params.id
+
+    Producto.findById(id, (err, productoDB)=>{
+        if(err){
+            return res.status(500).json({
+                ok: false,
+                err
+            })
+        }
+
+        if(!productoDB){
+            return res.status(400).json({
+                ok: false,
+                err:{
+                    message: 'El Id no existe'
+                }
+            })
+        }
+
+        productoDB.disponible = false
+
+        productoDB.save((err, productoEliminado)=>{
+            if(err){
+                return res.status(500).json({
+                    ok: false,
+                    err
+                })
+            }
+
+            res.json({
+                ok: true,
+                producto: productoEliminado,
+                mensaje: 'Producto Eliminado!!!'
+            })
+        })
+    })
+})
+
+
+module.exports = app
