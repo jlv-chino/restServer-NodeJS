a83e08e340eb5bad2745cc60ef5f8f7ee78adc94
José Luis Vilches <jose.luis.comvilches@gmail.com>
2021-04-15 01:58:21 -0300

    Ver imagen en sitio web mediante token

--- server/middlewares/autenticacion.js
+++ server/middlewares/autenticacion.js
@@ -37,7 +37,26 @@ let verificaAdmin_Role = (req, res, next) =>{
     
 }
 
+let verificaTokenImg = (req, res, next)=>{
+    let token = req.query.token
+
+    jwt.verify(token, process.env.SEED, (err, decoded) =>{
+        if (err){
+            return res.status(401).json({
+                ok: false,
+                err:{
+                    message: 'Token incorrecto'
+                }
+            })
+        }
+
+        req.usuario = decoded.usuario
+        next() 
+    })
+}
+
 module.exports = {
     verificaToken,
-    verificaAdmin_Role
+    verificaAdmin_Role,
+    verificaTokenImg
 }
--- server/routes/imagenes.js
+++ server/routes/imagenes.js
@@ -3,11 +3,11 @@ const express = require('express')
 const fs = require('fs')
 const path = require('path')
 
-const {verificaToken} = require('../middlewares/autenticacion')
+const {verificaTokenImg} = require('../middlewares/autenticacion')
 
 let app = express()
 
-app.get('/imagen/:tipo/:img', verificaToken,(req, res) =>{
+app.get('/imagen/:tipo/:img', verificaTokenImg,(req, res) =>{
     let tipo = req.params.tipo
     let img = req.params.img
 
